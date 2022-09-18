907d1966fee4f6ca00b283b680207e1de865a722
(Abner <(abner.amaral.silva@gmail.com)>
2022-09-18 11:54:27 -0300

    Criando Repositorio Git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'car-sales-project';
+  title = 'Bem vindo ao Curso de Angular';
 }
